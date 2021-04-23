efd819c4b18d9983ceb6231d6984860f380bf41c
Zett-8 <kame0008@gmail.com>
2021-04-24 00:10:38 +0200

    typescript

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,11 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 
-const Layout = ({ children }) => {
+interface Props {
+  children: any
+}
+
+const Layout = ({ children }: Props) => {
 
   return (
     <>
