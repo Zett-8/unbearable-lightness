b3c539e592dbde48a8ec222136952bca225a3567
Zett-8 <kame0008@gmail.com>
2021-11-10 16:41:15 +0100

    use rootWrapper for css

--- gatsby-wrapper.js
+++ gatsby-wrapper.js
@@ -3,13 +3,13 @@ import { CssReset } from './src/styles/cssReset'
 import { CustomGlobalCss } from './src/styles/customGlobalCss'
 
 // Set up any Provider components that will wrap the application.
-export const rootWrapper = ({ element }) => <>{element}</>
-
-// Set wrapper components around pages that won’t get unmounted on page changes.
-export const pageWrapper = ({ element }) => (
+export const rootWrapper = ({ element }) => (
   <>
     <CssReset />
     <CustomGlobalCss />
     {element}
   </>
 )
+
+// Set wrapper components around pages that won’t get unmounted on page changes.
+export const pageWrapper = ({ element }) => <>{element}</>
