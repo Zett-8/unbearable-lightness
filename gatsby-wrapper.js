9ad5d1445d0813e906e2093b9ccb46cba6933bc8
Zett-8 <kame0008@gmail.com>
2021-11-30 22:05:13 +0100

    set CSS reset in pageWrapper not in rootWrapper

--- gatsby-wrapper.js
+++ gatsby-wrapper.js
@@ -3,13 +3,14 @@ import { CssReset } from './src/styles/cssReset'
 import { CustomGlobalCss } from './src/styles/customGlobalCss'
 
 // Set up any Provider components that will wrap the application.
-export const rootWrapper = ({ element }) => (
+export const rootWrapper = ({ element }) => <>{element}</>
+
+// Set wrapper components around pages that won’t get unmounted on page changes.
+export const pageWrapper = ({ element }) => (
   <>
+    {/* Global CSS must belong here not in rootWrapper otherwise CSS loading delay occurs */}
     <CssReset />
     <CustomGlobalCss />
     {element}
   </>
 )
-
-// Set wrapper components around pages that won’t get unmounted on page changes.
-export const pageWrapper = ({ element }) => <>{element}</>
