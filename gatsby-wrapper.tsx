a77f27fa05711ee8ab0727c9cb35e8fe2f7cd744
Toshiki Kamei <kame0008@gmail.com>
2022-07-06 09:39:36 +0200

    update props

--- gatsby-wrapper.tsx
+++ gatsby-wrapper.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, JSXElementConstructor } from 'react'
 import { GatsbyBrowser, GatsbySSR } from 'gatsby'
 import { ThemeProvider } from 'styled-components'
 
@@ -6,10 +6,7 @@ import { CssReset } from './src/styles/cssReset'
 import { CustomGlobalCss } from './src/styles/customGlobalCss'
 import { theme } from './src/styles/theme'
 
-type Props = {
-  element: ReactElement
-}
-
+type Props = { element: ReactElement<any, string | JSXElementConstructor<any>> }
 type RootWrapper = GatsbyBrowser['wrapRootElement'] | GatsbySSR['wrapRootElement']
 type PageWrapper = GatsbyBrowser['wrapPageElement'] | GatsbySSR['wrapPageElement']
 
@@ -22,7 +19,7 @@ export const pageWrapper: PageWrapper = ({ element }: Props) => (
     {/* Global CSS must belong here not in rootWrapper otherwise CSS loading delay occurs */}
     <CssReset />
     <CustomGlobalCss />
-    {/*@ts-ignore*/}
+    {/*@ts-ignore TODO*/}
     <ThemeProvider theme={theme}>{element}</ThemeProvider>
   </>
 )
