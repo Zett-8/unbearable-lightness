1f9659568cdc65cd244929576c113b827c505dfa
Toshiki Kamei <kame0008@gmail.com>
2022-07-12 20:10:00 +0200

    refactor styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,22 @@
-export const theme = {}
+export const theme = {
+  color: {
+    theme: `hsl(0, 0%, 98%)`,
+
+    while: `white`,
+    backgroundWhite: `hsl(0, 0%, 98%)`,
+
+    black: `black`,
+    regularBlack: `hsl(0, 0%, 13%)`,
+
+    subTextGrey: `hsl(0, 0%, 46%)`,
+  },
+
+  font: {
+    weight: {
+      '300': 300,
+    },
+    family: {
+      sanSerif: `'Roboto', 'Helvetica', 'Hiragino Sans', 'Meiryo', sans-serif`,
+    },
+  },
+}
