6792c1877e5f9d4d8203d3ff3e5ee0badf566568
Toshiki Kamei <kame0008@gmail.com>
2022-08-22 11:57:48 +0200

    fix page wrapper

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,14 @@
 /** 1. Imports **/
 import React from 'react'
+import type { PageProps } from 'gatsby'
 import styled from 'styled-components'
 
 import { Header } from './header'
 import { Footer } from './footer'
 
 /** 2. Types **/
-type Props = {
+type Props = PageProps & {
   className?: string
-  children: React.ReactNode
 }
 
 /** 3. Base component **/
