68c01d0475272d78af928c9ca4b7b382a4e1eeb8
Toshiki Kamei <kame0008@gmail.com>
2022-08-22 15:46:02 +0200

    use head api

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,18 @@
 /** 1. Imports **/
 import React from 'react'
-import { PageProps } from 'gatsby'
-import { MetaTag } from '../components/metaTag'
+import { HeadProps, PageProps } from 'gatsby'
+import { SEO } from '../components/seo'
 
 /** 3. Base component **/
 const NotFoundPage = (props: PageProps) => (
   <>
-    <MetaTag title="404: Not found" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </>
 )
+
 export default NotFoundPage
+
+export const Head = (props: HeadProps) => {
+  return <SEO {...props} title={'404: Not found'} />
+}
